de41238f513e8946f3e82eff685e3c3d3c341957
Mayank Negi <itsmemayank02@gmail.com>
2024-07-24 01:21:24 +0530

    Updated configuration and fixed paths

--- public/js/client.js
+++ public/js/client.js
@@ -1,49 +1,46 @@
 const socket = io();
-
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('msginp');
 const messageContainer = document.querySelector(".container");
 const notificationSound = document.getElementById('notification-sound');
 
 const append = (message, position, user = '') => {
-  const messageElement = document.createElement('div');
-  const userElement = document.createElement('span');
-  const userName = document.createElement('b');
+    const messageElement = document.createElement('div');
+    const userElement = document.createElement('span');
+    const userName = document.createElement('b');
 
-  userName.innerText = user;
-  userElement.append(userName);
+    userName.innerText = user;
+    userElement.append(userName);
 
-  if (user) {
-    messageElement.append(userElement);
-    messageElement.append(document.createElement('br'));
-  }
+    if (user) {
+        messageElement.append(userElement);
+        messageElement.append(document.createElement('br'));
+    }
 
-  messageElement.append(message);
-  messageElement.classList.add('msg');
-  messageElement.classList.add(position);
-  messageContainer.append(messageElement);
+    messageElement.append(message);
+    messageElement.classList.add('msg');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
 
-  // Play sound for incoming messages or user joins
-  if (position === 'left' || position === 'middle') {
+    // Play notification sound
     notificationSound.play();
-  }
 };
 
 const user = prompt('Enter your name to join');
 socket.emit('new-user-joined', user);
 
 socket.on('user-connected', user => {
-  append(`${user} joined the chat`, 'middle');
+    append(`${user} joined the chat`, 'middle');
 });
 
 socket.on('receive-message', data => {
-  append(data.message, 'left', data.user);
+    append(data.message, 'left', data.user);
 });
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const message = messageInput.value;
-  append(` ${message}`, 'right');
-  socket.emit('send-message', { message: message });
-  messageInput.value = '';
+    e.preventDefault();
+    const message = messageInput.value;
+    append(` ${message}`, 'right');
+    socket.emit('send-message', { message: message });
+    messageInput.value = '';
 });
